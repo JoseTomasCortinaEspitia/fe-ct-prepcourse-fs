3f64ca14496f284fadc078a26374b302ece85589
Jose Tomas Cortina Espitia <joseto.corties@gmail.com>
2023-03-03 16:50:12 -0500

    Módulo 08

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,13 +5,14 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
-   return 
+   return nombre[0].toUpperCase() + nombre.substring(1);
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -19,6 +20,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1, num2)
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -27,6 +29,13 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   var suma = 0;
+   var i = 0;
+   while (i<arrayOfNumbers.length) {
+      suma += arrayOfNumbers[i]
+      i += 1
+   }
+   cb(suma)
 }
 
 function forEach(array, cb) {
@@ -34,6 +43,11 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   i= 0;
+   while (i<array.length){
+      cb(array[i])
+      i += 1
+   }
 }
 
 function map(array, cb) {
@@ -51,6 +65,15 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var resultado = [];
+   i = 0;
+   while (i<arrayOfStrings.length) {
+      if (arrayOfStrings[i].charAt(0) == "a"){
+         resultado.push(arrayOfStrings[i])
+      }
+      i += 1
+   }
+   return resultado
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
